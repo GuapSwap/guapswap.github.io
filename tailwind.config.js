03bf5e75ef74a9b092a2d4d65d1125808a753be5
Luca D'Angelo <ldgaetano@protonmail.com>
2022-11-27 00:09:29 -0500

    starting landing page build using tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      boxShadow: {
+        'guap': '0px 0px 10px 2px rgba(0, 0, 0, 1)'
+      }
+    },
+  },
+  plugins: [],
+}
