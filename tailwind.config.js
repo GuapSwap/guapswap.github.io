2d469ed023a0fc16648e116f41fdbd7730f792d9
Luca D'Angelo <ldgaetano@protonmail.com>
2022-12-22 00:32:45 -0500

    finished website design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html'],
+  content: ['../index.html'],
   theme: {
     extend: {
       boxShadow: {
-        'guap': '0px 0px 10px 2px rgba(0, 0, 0, 1)'
+        'guap': '0px 0px 10px 2px rgba(0, 0, 0, 0)'
       }
     },
   },
